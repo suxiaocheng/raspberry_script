7a7332936918ac864b78a32e5a8e8ba89c17dafd
suxiaocheng <suxiaocheng2010@gmail.com>
2018-10-03 08:34:44 +0800

    Fix ngrok script

--- ngrok/ngrok_start.sh
+++ ngrok/ngrok_start.sh
@@ -4,7 +4,8 @@ ngrok="/home/pi/program/ngrok/ngrok"
 
 if [ -f $ngrok ]; then
         export HOME=/home/pi
-        $ngrok tcp 22 -log=stdout -log-level=debug &
+        # $ngrok tcp 22 -log=stdout -log-level=debug &
+	$ngrok http 80 -log=stdout -log-level=debug &
 else
         echo "File not found: " $ngrok
 fi
--- ngrok/ngrok_stop.sh
+++ ngrok/ngrok_stop.sh
@@ -5,13 +5,14 @@ PID_PROGRAM=`ps -aux |grep "/home/pi/program/ngrok/ngrok "|grep -v "grep" | awk 
 # echo $PID  
 # echo $PID_PROGRAM
 
-if [ -n $PID ]; then
-        kill -9 $PID
+if [ -n "$PID" ]; then
+	echo "Going to kill process (${PID})"
+        kill $PID
 else
         echo "sh file is already exit"
 fi
 
-if [ -n $PID_PROGRAM ]; then
+if [ -n "$PID_PROGRAM" ]; then
         kill -9 $PID_PROGRAM
 else
         echo "bin file is already exit"
