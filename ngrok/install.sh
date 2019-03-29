782c40c7cd3659efe42d9c7d3b0119c102174eb3
suxiaocheng <suxiaocheng2010@hotmail.com>
2019-03-29 23:05:08 +0800

    Upate the binary file loc
    Unzip the binary from zip archive. And change the install script
    to install the binary to the /usr/bin dir.

--- ngrok/install.sh
+++ ngrok/install.sh
@@ -1,6 +1,13 @@
 
 systemctl stop ngrok.service
 
+cp bin/ngrok /usr/bin/
+
+if [ $? != "0" ]; then
+	echo "Copy binary to /usr/bin fail"
+	exit 0
+fi
+
 cp ngrok.service /usr/lib/systemd/system/
 sync
 
