5295e292fa94c665f4dce26a19b08e34925376ec
suxiaocheng <suxiaocheng2010@gmail.com>
2019-04-15 12:28:54 +0800

    Add code protect

---
+++ baddha/install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+check_str="animal"
+
+# Add execute environment var
+env_status=`cat ~/.bashrc | grep ${check_str}`
+
+if [ -z "${env_status}" ];then
+	echo "${check_str} is not set, insert it"
+	cp ~/.bashrc ~/.bashrc.bak
+	echo "if [ \"\${0}\" = \"-bash\" ]; then
+ echo \"　　　　　　　 ┏┓       ┏┓+ +\"
+ echo \"　　　　　　　┏┛┻━━━━━━━┛┻┓ + +\"
+ echo \"　　　　　　　┃　　　　　　 ┃\"
+ echo \"　　　　　　　┃　　　━　　　┃ ++ + + +\"
+ echo \"　　　　　　 █████━█████  ┃+\"
+ echo \"　　　　　　　┃　　　　　　 ┃ +\"
+ echo \"　　　　　　　┃　　　┻　　　┃\"
+ echo \"　　　　　　　┃　　　　　　 ┃ + +\"
+ echo \"　　　　　　　┗━━┓　　　 ┏━┛\"
+ echo \"                 ┃　　  ┃\"
+ echo \"　　　　　　　　　┃　　  ┃ + + + +\"
+ echo \"　　　　　　　　　┃　　　┃　Code is far away from     bug with the animal protecting\"
+ echo \"　　　　　　　　　┃　　　┃ +              神兽保佑,代码无bug\"
+ echo \"　　　　　　　　　┃　　　┃\"
+ echo \"　　　　　　　　　┃　　　┃　　+\"
+ echo \"　　　　　　　　　┃　 　 ┗━━━┓ + +\"
+ echo \"　　　　　　　　　┃ 　　　　　┣┓\"
+ echo \"　　　　　　　　　┃ 　　　　　┏┛\"
+ echo \"　　　　　　　　　┗┓┓┏━━━┳┓┏┛ + + + +\"
+ echo \"　　　　　　　　　 ┃┫┫　 ┃┫┫\"
+ echo \"　　　　　　　　　 ┗┻┛　 ┗┻┛+ + + +\"
+fi" >> ~/.bashrc
+fi
+
+echo "sucessfully"
