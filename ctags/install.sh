e72a54156f6ccde194245a3a0a2f799415a7e345
suxiaocheng <suxiaocheng2010@gmail.com>
2019-04-17 17:15:21 +0800

    ctags add auto install package

--- ctags/install.sh
+++ ctags/install.sh
@@ -3,6 +3,20 @@
 binary_name="update_ctags.sh"
 binary_dir="$HOME""/bin"
 
+
+CTAGS_PROGRAM=`which ctags`
+CSCOPE_PROGRAM=`which cscope`
+
+if [ -z "$CTAGS_PROGRAM" -a -z "$CSCOPE_PROGRAM" ]; then
+	sudo apt install -y ctags cscope
+	if [[ "$?" -ne 0 ]]; then
+		echo "ctags install fail"
+		exit 1
+	else
+		echo "ctags is not exist, install ok"
+	fi
+fi
+
 if [ ! -d "${binary_dir}" ]; then
 	echo "${binary_dir} directory is not exist"
 	mkdir "${binary_dir}"
@@ -12,8 +26,14 @@ if [ ! -f ${binary_dir}"/"${binary_name} ]; then
 	echo "${binary_name} is not exist"
 fi
 
+if [ -f "${binary_dir}/.vimrc" ]; then
+	echo "backup vimrc file"
+	cp ${HOME}/.vimrc ${HOME}/.vimrc.bak
+fi
+
 cp ${binary_name} ${binary_dir}"/"${binary_name}
 chmod 0777 ${binary_dir}"/"${binary_name}
+cp .vimrc ${HOME}"/.vimrc"
 
 # Add execute environment var
 env_status=`cat ~/.bashrc | grep ${binary_dir}`
