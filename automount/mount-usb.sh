165de4bb7783db941b23733e5b535223304eba86
suxiaocheng <suxiaocheng2010@hotmail.com>
2019-04-25 23:49:32 +0800

    [automount] Add script to check automount

--- automount/mount-usb.sh
+++ automount/mount-usb.sh
@@ -1,7 +1,34 @@
 #!/bin/sh
+
+LOG_FILE=/var/log/tmp.txt
+
+echo "**Going to mount $1" >> $LOG_FILE
+
+id -a >> $LOG_FILE
+
 if [ ! -d /media/$1 ]; then
 	mkdir -p /media/$1
 	chown pi:pi /media/$1
 fi
-/bin/mount -t vfat /dev/$1 /media/$1
-sync
+/bin/mount -t vfat -o gid=1000,uid=1000 /dev/$1 /media/$1 2>>$LOG_FILE  >> $LOG_FILE
+
+if [ "$?" -eq "0" ]; then
+	# printout the mount information
+	count=0
+	while [ ${count} -le 20 ]
+	do
+		dat=`mount |grep "$1"`
+		if [ -n ${dat} ]; then
+			echo "info: ${dat}" >> $LOG_FILE
+			break
+		else
+			echo "info: still trying ${count}" >> $LOG_FILE
+		fi
+		count=$(($count+1))
+		sleep 1
+	done
+	echo "mount $1 sucessfully" >> $LOG_FILE
+else
+	echo "mount $1 fail" >> $LOG_FILE
+fi
+
--- automount/umount-usb.sh
+++ automount/umount-usb.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
+LOG_FILE=/var/log/tmp.txt
+
+echo "**Going to umount $1" >> $LOG_FILE
+
 # check if directory is mount
 dat=`mount |grep "$1"`
 
-if [ "$?" -eq 1 ]; then
-        echo "Not mount: " $1
+
+if [ -z "$dat"]; then
+        echo "Not mount: " $1 >> $LOG_FILE
+	if [ -d /media/$1 ]; then
+		rm -rf /media/$1
+		echo "remove exist dir /media/$1" >> $LOG_FILE
+	fi
 else
-        /bin/umount /dev/$1
+	echo "info: $dat" >> $LOG_FILE
+        /bin/umount /dev/$1 2>>$LOG_FILE >>$LOG_FILE
         if [ "$?" = "0" ]; then
                 rm -rf /media/$1
+	else
+		echo "umount /dev/$1 fail" >> $LOG_FILE
         fi  
-        sync
 fi
