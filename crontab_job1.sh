a8aea5ccb28786e655a8e9f79597116cc9ad2af2
suxiaocheng <suxiaocheng2010@hotmail.com>
2019-07-20 09:54:56 +0800

    crontab job update

--- crontab_job1.sh
+++ crontab_job1.sh
@@ -10,23 +10,32 @@ CHECK_DIR[1]="vim_plugin"
 
 
 if [ ! -d ${RASP_CONFIG_DIR} ]; then
-	exit 1
+	RASP_CONFIG_DIR=~/program/shell/raspberry_script
+	if [ ! -d ${RASP_CONFIG_DIR} ]; then
+		echo "program dir ${RASP_CONFIG_DIR} not exist"
+		exit 1
+	fi
 fi
 
 cd ${RASP_CONFIG_DIR}
 git pull
 git merge origin/master
 
+echo "************************"
+echo "date: ${current_date}"
+echo "************************"
+
 for ((i=0; i< ${#CHECK_DIR[*]}; i++)); do
         echo "+++++++++begin++++++++++"
-	echo "date: ${current_date}"
 	echo ${CHECK_DIR[i]}
-        echo "========================"
 	BACKPWD=`pwd`
 	cd ${CHECK_DIR[i]}
 	./install.sh
 	cd ${BACKPWD}
         echo "++++++++++end+++++++++++"
 done
+
+echo ""
+echo ""
 
 
