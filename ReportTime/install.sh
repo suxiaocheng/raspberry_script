de321d89f74ab9aeaf7d15641df7d3d47623eb80
suxiaocheng <suxiaocheng2010@hotmail.com>
2019-09-08 17:04:11 +0800

    ReportTime: make the work enviroement more depent

--- ReportTime/install.sh
+++ ReportTime/install.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 SERVICE_NAME=report_time.service
+START_SCRIPT_NAME=start.sh
+STOP_SCRIPT_NAME=stop.sh
 SED_STRING="s/myuser/${USER}/g"
 
 sed ${SED_STRING} ${SERVICE_NAME}.tmpl > ${SERVICE_NAME}
-exit 0
+sed ${SED_STRING} ${START_SCRIPT_NAME}.tmpl > ${START_SCRIPT_NAME}
+sed ${SED_STRING} ${STOP_SCRIPT_NAME}.tmpl > ${STOP_SCRIPT_NAME}
 
 sudo systemctl stop ${SERVICE_NAME}
 
 cp -r ../ReportTime ~
 
 if [ -d "/usr/lib/systemd/system/" ]; then
-        mv ${SERVICE_NAME} /usr/lib/systemd/system/
+        sudo mv ${SERVICE_NAME} /usr/lib/systemd/system/
 elif [ -d "/etc/systemd/system" ]; then
-        mv ${SERVICE_NAME} /etc/systemd/system/
+        sudo mv ${SERVICE_NAME} /etc/systemd/system/
 else
         echo "Install service[${SERVICE_NAME}] fail"
         exit 1
--- ReportTime/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh  
-PID=$(cat /var/run/ReportTime.pid)  
-kill -9 $PID 
