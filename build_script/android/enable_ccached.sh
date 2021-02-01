ba330d1ce8116ef22df26f4621e2fbd1ac20bcf8
xiaocheng.su <xiaocheng.su@tcl.com>
2021-02-01 08:57:01 +0800

    build_script: fix build script error

--- build_script/android/enable_ccached.sh
+++ build_script/android/enable_ccached.sh
@@ -6,8 +6,8 @@ export CCACHE_DIR=$(pwd)
 
 echo "Setting ccache dir to ${CCACHE_DIR}"
 
-CCACHE_SCRIPT=prebuilt/linux-x86/ccache/ccache
-if [ -f -x ${CCACHE_SCRIPT} ]; then
+CCACHE_SCRIPT=$(which ccache)
+if [ -f ${CCACHE_SCRIPT} -a -x ${CCACHE_SCRIPT} ]; then
 	${CCACHE_SCRIPT} -M 30G 
 else
 	echo "[ERROR] cached file ${CCACHE_SCRIPT} is not exit!!!"
