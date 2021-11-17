3d665a21be2786522f942f98cbfb5aff8c7a8ed7
suxiaocheng <suxiaocheng2010@gmail.com>
2021-11-17 09:42:29 +0800

    vim_plugin: add retry.sh script to retry command

---
+++ vim_plugin/retry.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+count=0
+if [ $# -gt 0 ]; then
+	# echo "[INFO] arg is max than zero"
+	if [ -n $1 ]; then
+		while [ true ]; do
+			echo "[INFO] Execute command: "$1 " ,times "$count
+			${1}
+			if [ $? -eq 0 ]; then
+				break;
+			fi
+			count=$((count+1))
+			if [ $# -ge 2 ]; then
+				if grep '^[[:digit:]]*$' <<< "$2";then
+					echo "[INFO] Sleep $2"
+					sleep $2
+				fi
+			fi
+		done
+	fi
+else
+	echo "[ERR] arg is null"
+fi
