81f1f7e25d3a5a3b7304adbfb0af93865afcb506
su.xiaocheng <su.xiaocheng@byd.com>
2022-08-16 08:35:19 +0800

    tool: add cgdb relate tool

--- tool/install.sh
+++ tool/install.sh
@@ -4,3 +4,5 @@ sudo apt install -y nmap vim cargo samba
 sudo apt install -y libglib2.0-dev  libpixman-1-dev
 sudo apt install -y ffmpeg motion
 sudo apt install -y flex bison libncurses-dev
+sudo apt install -y gdb-multiarch
+sudo apt install -y automake texinfo
