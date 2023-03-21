3b2381e4271cf4968009dec536a220adecaecee3
suxiaocheng <suxiaocheng2010@hotmail.com>
2023-03-21 21:32:30 +0800

    tool: fix cat_dir.sh absoulate path error

--- tool/cat_dir.sh
+++ tool/cat_dir.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-if [ -n $1 ]; then
-	dir=$1
+if [ -n "$1" ]; then
+	dir=`readlink -f $1`
 else
 	dir=`pwd`
 fi
 
-for f in `ls $dir`; do if [ -f $f  ]; then if [ -r $f ]; then  echo $f":"; cat $f; fi; fi;  done
+echo "[INFO] Current working dir is: $dir"
+
+for f in `ls $dir`; do 
+	file=${dir}/$f
+	if [ -f ${file}  ]; then 
+		if [ -r ${file} ]; then 
+			linecount=`wc -l ${file} | awk '{print $1}'`
+			echo "[INFO] ${file} [$linecount]:"; 
+			cat ${file}; 
+			if [ -n "$linecount" ]; then
+				if [ $linecount -gt 0 ]; then
+					echo ""
+				fi
+			fi
+		fi; 
+	fi;  
+done
+
